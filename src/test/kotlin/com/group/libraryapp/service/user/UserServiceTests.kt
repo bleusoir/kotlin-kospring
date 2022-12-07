af80e2798dd75730cd86a458e106e43ec2a038b6
jypark <bleusoire@gmail.com>
2022-12-07 23:23:36 +0900

    사용자 조회 테스트 추가

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTests.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTests.kt
@@ -1,8 +1,10 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,6 +15,11 @@ open class UserServiceTests @Autowired constructor(
     private val userRepository: UserRepository,
     private val userService: UserService,
 ) {
+
+    @AfterEach
+    fun clean() {
+        userRepository.deleteAll()
+    }
 
     @Test
     @Transactional
@@ -29,4 +36,23 @@ open class UserServiceTests @Autowired constructor(
         assertThat(results).hasSize(1)
         assertThat(results[0].age).isNull()
     }
+
+    @Test
+    fun getUsersTest() {
+
+        // given
+        userRepository.saveAll(listOf(
+            User("A", 20),
+            User("B", null),
+        ))
+
+        // when
+        val results = userService.getUsers()
+
+        // then
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("A", "B")
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(20, null)
+    }
+
 }
